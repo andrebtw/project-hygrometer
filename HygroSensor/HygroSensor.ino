c44f2152af20a44c5b7bf828543643cb1487d7a2
andrebtw <43089870+andrebtw@users.noreply.github.com>
2021-12-09 09:39:32 +0100

    LCD Prints the values on the hygrometer.

--- HygroSensor/HygroSensor.ino
+++ HygroSensor/HygroSensor.ino
@@ -1,14 +1,44 @@
 #define SensorPin A0
+#include <LiquidCrystal.h>;
+
+
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 
 float sensorValue = 0;
 
+int contrast=60 ;
+
+String condition;
 
 void setup() { 
- Serial.begin(9600); 
+  analogWrite(6, contrast);
+  lcd.begin(16, 2);
+  Serial.begin(9600);
 } 
 
 
-void loop() { 
+
+void loop() {
   sensorValue = analogRead(SensorPin); 
   Serial.println(sensorValue);
+  if (sensorValue >= 0 and sensorValue <= 35){
+    condition="Air ambient";
+    }
+  if (sensorValue > 36 and sensorValue <= 300){
+    condition="Sol sec";
+    
+    }
+  if (sensorValue > 301 and sensorValue <= 700){
+    condition="Sol humide";
+    
+    }
+  if (sensorValue > 701 and sensorValue <= 950){
+    condition="Immerger";
+    
+    }
+
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print(condition);
+  delay(250);
 }
